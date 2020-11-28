9a29c3d9b26610d28807c0a92702227f326b7d09
Priya Rajput <rajputpriya560@gmail.com>
2020-11-28 14:00:34 +0530

    added splash screen

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import { green } from './common/Colors';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        style='auto'
+        backgroundColor={green}
+        barStyle='light-content'
+      />
     </View>
   );
 }
---
+++ common/Colors.js
@@ -0,0 +1,6 @@
+export const darkGreen = '#206a5d';
+export const green = '#81b214';
+export const lightGreen = '#bfdcae';
+export const offWhite = '#f1f1e8';
+export const black = '#3d3d3d';
+export const white = '#fff';
